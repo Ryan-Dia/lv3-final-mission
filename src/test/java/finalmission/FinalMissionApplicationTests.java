03414774013dcbfd4aba0f2ce8ffd0340c99b891
ryan-dia <cjfdnjs158@naver.com>
2025-09-03 14:20:55 +0900

    test: activeProfiles 어노테이션 추가

--- src/test/java/finalmission/FinalMissionApplicationTests.java
+++ src/test/java/finalmission/FinalMissionApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestClient;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinalMissionApplicationTests {
 
   @Autowired
